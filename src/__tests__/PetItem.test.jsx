11832712a59ee0ef5e9b92762662b0fbc06906ca
Victor Volovik <VictorVolovik@users.noreply.github.com>
2024-08-21 16:30:55 +0400

    Setup vitest & add PetItems tests

---
+++ src/__tests__/PetItem.test.jsx
@@ -0,0 +1,26 @@
+import { expect, test } from "vitest";
+import { render } from "@testing-library/react";
+import PetItem from "../components/PetItem";
+import { StaticRouter } from "react-router-dom/server";
+
+test("displays a default thumbnail", async () => {
+  const petItem = render(
+    <StaticRouter>
+      <PetItem />
+    </StaticRouter>
+  );
+  const petThumbnail = await petItem.findByTestId("thumbnail");
+  expect(petThumbnail.src).toContain("none.jpg");
+  petItem.unmount();
+});
+
+test("displays a non-default thumbnail", async () => {
+  const petItem = render(
+    <StaticRouter>
+      <PetItem images={["1.jpg", "2.jpg", "3.jpg"]} />
+    </StaticRouter>
+  );
+  const petThumbnail = await petItem.findByTestId("thumbnail");
+  expect(petThumbnail.src).toContain("1.jpg");
+  petItem.unmount();
+});
--- src/components/PetItem.tsx
+++ src/components/PetItem.tsx
@@ -7,7 +7,7 @@ interface PetItemProps {
   name: string;
   animal: Animal;
   breed: string;
-  images: string[];
+  images?: string[];
   location: string;
 }
 
@@ -21,7 +21,7 @@ const PetItem = ({
 }: PetItemProps) => {
   let hero = `${IMAGES_URL}/pets/none.jpg`;
 
-  if (images.length) {
+  if (images?.length) {
     hero = images[0];
   }
 
@@ -29,7 +29,12 @@ const PetItem = ({
     <li className="relative block">
       <Link to={`/details/${id}`} className="flex h-full w-full">
         <div className="flex h-full  w-full">
-          <img className="object-cover" src={hero} alt={name} />
+          <img
+            data-testid="thumbnail"
+            className="object-cover"
+            src={hero}
+            alt={name}
+          />
         </div>
         <div className="absolute bottom-0 left-0 w-full bg-gradient-to-tr from-white to-transparent p-2">
           <h2>{name}</h2>
